19f54a69b2b53bb14347752e88b1cd3f0578ca67
GitHub Action <action@github.com>
2020-09-24 13:26:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/panlufei/allset/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/panlufei/allset/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.panlufei.allset.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.panlufei.allset.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
